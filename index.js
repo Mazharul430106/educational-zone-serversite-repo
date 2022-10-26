74682aa307eef2405592df3125c33aad9b21990d
Mazharul Islam <mazharul430@gmail.com>
2022-10-25 23:32:03 -0700

    modify data.json file

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 5000;
+app.use(cors());
+
+const data = require('./data/data.json');
+
 
 app.get('/', (req, res) => {
   res.send('Hello Educanal Server Site !!')
 })
+
+
+app.get('/data', (req, res)=>{
+  res.send(data);
+})
+
 
 app.listen(port, () => {
   console.log(`Educanal Server Site Running on port ${port}`)
